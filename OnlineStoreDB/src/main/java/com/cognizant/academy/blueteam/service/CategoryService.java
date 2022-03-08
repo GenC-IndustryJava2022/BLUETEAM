5f3345500745da9f9e552fa92aa00546c2ae0bc2
Taylor Smith <tavlor@protonmail.com>
2022-03-08 17:29:25 -0600

    started fixing categoryservice

--- OnlineStoreDB/src/main/java/com/cognizant/academy/blueteam/service/CategoryService.java
+++ OnlineStoreDB/src/main/java/com/cognizant/academy/blueteam/service/CategoryService.java
@@ -10,108 +10,81 @@ import java.util.List;
 
 import javax.sql.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementSetter;
 
 import com.cognizant.academy.blueteam.models.Category;
 import com.cognizant.academy.blueteam.repository.CategoryRepository;
 
-public class CategoryService implements CategoryRepository {
-	private static final String JDBC_URL="jdbc:derby:C:\\Users\\955514\\OnlineStore\\Store;create=true";
-	private Connection conn;
-	private DataSource dataSource;
-	private JdbcTemplate jdbcTemplate;
-	
-	
-public Connection getConn() {
-	return conn;
-}
+public class CategoryService {
 
-public void setConn(Connection conn) {
-	this.conn = conn;
-}
+	@Autowired
+	private CategoryRepository categoryRepository;
 
-public DataSource getDataSource() {
-	return dataSource;
-}
-
-public void setDataSource(DataSource dataSource) {
-	this.dataSource = dataSource;
-}
-
-public JdbcTemplate getJdbcTemplate() {
-	return jdbcTemplate;
-}
-
-public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-	this.jdbcTemplate = jdbcTemplate;
-}
-
-public static String getJdbcUrl() {
-	return JDBC_URL;
-}
 
 	@Override
 	public void insert(Category C) {
 		// TODO Auto-generated method stub
-		String sql="Insert into CATEGORY (CATEGORY_ID,NAME,HEADER_IMAGE) values (?,?,?)";
-		jdbcTemplate.update(sql,new PreparedStatementSetter() {
+		String sql = "Insert into CATEGORY (CATEGORY_ID,NAME,HEADER_IMAGE) values (?,?,?)";
+		jdbcTemplate.update(sql, new PreparedStatementSetter() {
 			@Override
-			public void setValues(PreparedStatement ps) throws SQLException{
+			public void setValues(PreparedStatement ps) throws SQLException {
 				ps.setInt(1, C.getCategoryId());
 				ps.setString(2, C.getName());
 				ps.setString(3, C.getHeaderImage());
 			}
 		});
 	}
+
 //UPDATE Customers
-	//SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'
-		//	WHERE CustomerID = 1;
+	// SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'
+	// WHERE CustomerID = 1;
 	@Override
 	public void update(Category C) {
-		String sql="UPDATE CATEGORY SET CATEGORY_ID=?, NAME=?, HEADER_IMAGE=? WHERE CATEGORY_ID=?";
-		jdbcTemplate.update(sql,new PreparedStatementSetter() {
+		String sql = "UPDATE CATEGORY SET CATEGORY_ID=?, NAME=?, HEADER_IMAGE=? WHERE CATEGORY_ID=?";
+		jdbcTemplate.update(sql, new PreparedStatementSetter() {
 			@Override
-			public void setValues(PreparedStatement ps) throws SQLException{
+			public void setValues(PreparedStatement ps) throws SQLException {
 				ps.setInt(1, C.getCategoryId());
 				ps.setString(2, C.getName());
 				ps.setString(3, C.getHeaderImage());
 				ps.setInt(4, C.getCategoryId());
 			}
-		});	
-		
+		});
+
 	}
 
 	@Override
 	public void delete(Category C) {
 		// TODO Auto-generated method stub
-		String sql="DELETE from CATEGORY WHERE CATEGORY_ID=?";
-		jdbcTemplate.update(sql,new PreparedStatementSetter() {
+		String sql = "DELETE from CATEGORY WHERE CATEGORY_ID=?";
+		jdbcTemplate.update(sql, new PreparedStatementSetter() {
 			@Override
-			public void setValues(PreparedStatement ps) throws SQLException{
+			public void setValues(PreparedStatement ps) throws SQLException {
 				ps.setInt(1, C.getCategoryId());
 			}
 		});
 	}
 
-	public List<Category> findAll(){
+	public List<Category> findAll() {
 		try {
-			ArrayList<Category> list=new ArrayList<>();
-			Statement stmt=jdbcTemplate.getDataSource().getConnection().createStatement();
-			String select="SELECT * FROM CATEGORY";
-			ResultSet rs= stmt.executeQuery(select);
-			while(rs.next()) {
-				int ID= rs.getInt("CATEGORY_ID");
-				String name=rs.getString("NAME");
-				String Himage=rs.getString("HEADER_IMAGE");
-				list.add(new Category(ID,name,Himage));
+			ArrayList<Category> list = new ArrayList<>();
+			Statement stmt = jdbcTemplate.getDataSource().getConnection()
+					.createStatement();
+			String select = "SELECT * FROM CATEGORY";
+			ResultSet rs = stmt.executeQuery(select);
+			while (rs.next()) {
+				int ID = rs.getInt("CATEGORY_ID");
+				String name = rs.getString("NAME");
+				String Himage = rs.getString("HEADER_IMAGE");
+				list.add(new Category(ID, name, Himage));
 			}
 			return list;
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
-	
+
 }
