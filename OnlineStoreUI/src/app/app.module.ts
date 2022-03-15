d9a8e25777547164ba354b0fed80a4a771abb2cf
Taylor Smith <tavlor@protonmail.com>
2022-03-15 15:18:04 -0500

    initialize anticipated components

--- OnlineStoreUI/src/app/app.module.ts
+++ OnlineStoreUI/src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { SearchFieldComponent } from './search-field/search-field.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { CategoryViewComponent } from './category-view/category-view.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductPreviewComponent } from './product-preview/product-preview.component';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { CartListComponent } from './cart-list/cart-list.component';
+import { CartItemComponent } from './cart-item/cart-item.component';
+import { CheckoutFormComponent } from './checkout-form/checkout-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    SearchFieldComponent,
+    HomePageComponent,
+    CategoryViewComponent,
+    ProductsListComponent,
+    ProductPreviewComponent,
+    SearchPageComponent,
+    ProductPageComponent,
+    CartPageComponent,
+    CartListComponent,
+    CartItemComponent,
+    CheckoutFormComponent
   ],
   imports: [
     BrowserModule,
