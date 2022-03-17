6c0287ea19145e8539ba887dca815cea1fad2e48
Taylor Smith <tavlor@protonmail.com>
2022-03-17 18:47:05 -0500

    cart list shows active product details

--- OnlineStoreUI/src/app/cart-list/cart-list.component.ts
+++ OnlineStoreUI/src/app/cart-list/cart-list.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActiveProduct } from '../active-product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-cart-list',
   templateUrl: './cart-list.component.html',
-  styleUrls: ['./cart-list.component.css']
+  styleUrls: ['./cart-list.component.css'],
 })
 export class CartListComponent implements OnInit {
+  @Input() activeProduct!: ActiveProduct;
 
-  constructor() { }
+  constructor(private productService: ProductService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  isActiveProductDefined(
+    maybeAActiveProduct: ActiveProduct = this.activeProduct
+  ): boolean {
+    return Boolean(maybeAActiveProduct);
   }
-
 }
