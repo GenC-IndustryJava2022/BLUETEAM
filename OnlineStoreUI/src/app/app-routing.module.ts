728f420f1a78541d4f385262700fc33b014e80f5
Taylor Smith <tavlor@protonmail.com>
2022-03-16 19:18:23 -0500

    set up menu bar

--- OnlineStoreUI/src/app/app-routing.module.ts
+++ OnlineStoreUI/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomePageComponent},
+  {path:'search', component: SearchPageComponent},
+  {path:'product/:id', component: ProductPageComponent},
+  {path:'cart', component: CartPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
