972627c1d594dc99a0e9407e483e1e423e946104
Taylor Smith <tavlor@protonmail.com>
2022-03-17 20:22:42 -0500

    start using products-list

--- OnlineStoreUI/src/app/products-list/products-list.component.ts
+++ OnlineStoreUI/src/app/products-list/products-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-products-list',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products-list.component.css']
 })
 export class ProductsListComponent implements OnInit {
-
+  @Input() public products!:Product[];
   constructor() { }
 
   ngOnInit(): void {
