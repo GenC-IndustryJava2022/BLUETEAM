b63c52fdfe2b8b7b3a26fc63dd2a59d559436476
Taylor Smith <tavlor@protonmail.com>
2022-03-17 19:09:41 -0500

    add quantity form and prepare for updating value

--- OnlineStoreUI/src/app/cart-list/cart-list.component.ts
+++ OnlineStoreUI/src/app/cart-list/cart-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActiveProduct } from '../active-product';
+import { Product } from '../product';
 import { ProductService } from '../product.service';
 
 @Component({
@@ -9,14 +10,23 @@ import { ProductService } from '../product.service';
 })
 export class CartListComponent implements OnInit {
   @Input() activeProduct!: ActiveProduct;
+  product!: Product;
 
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {}
 
+  updateQuantity() {
+    console.log('updated quantity:' + this.activeProduct.quantity);
+  }
+
   isActiveProductDefined(
     maybeAActiveProduct: ActiveProduct = this.activeProduct
   ): boolean {
     return Boolean(maybeAActiveProduct);
+  }
+
+  isProductDefined(maybeAProduct: Product = this.product): boolean {
+    return Boolean(maybeAProduct);
   }
 }
