6724a85d973634dd0126af10976c272c4380394a
Taylor Smith <tavlor@protonmail.com>
2022-03-18 18:25:13 -0500

    fixes nonterminal error when starting DB app

--- OnlineStoreDB/src/main/java/com/cognizant/academy/blueteam/OnlineStoreDbApplication.java
+++ OnlineStoreDB/src/main/java/com/cognizant/academy/blueteam/OnlineStoreDbApplication.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.cognizant.academy.blueteam.models.*;
@@ -18,23 +19,25 @@ public class OnlineStoreDbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineStoreDbApplication.class, args);
-		ApplicationContext context= new ClassPathXmlApplicationContext("beans.xml");
+//		ApplicationContext context= new ClassPathXmlApplicationContext("beans.xml");
+		// ConfigurableApplicationContext context = SpringApplication.run(OnlineStoreDbApplication.class, args);
+
 //		CategoryService C= (CategoryService) context.getBean("CategoryDAO");
 //		JdbcProductDAO P= (JdbcProductDAO) context.getBean("ProductDAO");
 //		JdbcCartDAO Car= (JdbcCartDAO) context.getBean("CartDAO");
 //		JdbcActiveProductsDAO A= (JdbcActiveProductsDAO) context.getBean("ActiveProductsDAO");
-//		
+//
 //		Category categoryObj= new Category(15,"new category","headerimage");
 //		Product productObj=new Product(24,"AAA laptop",2500000,"image","if anything undervalued",categoryObj.getCategoryId());
 //		Cart cartObj= new Cart(38,"google chrome info");
 //		ActiveProducts APObj=new ActiveProducts(1,cartObj.getCartId(),productObj.getProductId(),13);
-		
+
 		//C.insert(categoryObj);
 		//P.insert(productObj);
 		//Car.insert(cartObj);
 		//A.insert(APObj);
-		
-		
+
+
 		//-----------
 		//categoryObj.setName("other category");
 		//categoryObj.setHeaderImage("updated headerImage");
@@ -47,17 +50,17 @@ public class OnlineStoreDbApplication {
 		//Car.update(cartObj);
 		//A.update(APObj);
 		//--------
-		
+
 		//List<Product> list= P.findAll();
 		//Collections.sort(list);
 		//System.out.println(list);
-		
-		
+
+
 		//A.delete(APObj);
 		//Car.delete(cartObj);
 		//P.delete(productObj);
 		//C.delete(categoryObj);
-		
+
 //		System.out.println(C.findAll());
 //		System.out.println(P.findAll());
 //		System.out.println(Car.findAll());
